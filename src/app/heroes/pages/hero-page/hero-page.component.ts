6bf1f58ad395757c2c2169af048a8b262518ee6b
Erick Daniel <erick.olano@gmail.com>
2024-11-15 01:09:26 -0500

    cap. 201 - Página del Héroe y refactorizando algunos estilos predefinidos que vienen en la librería PrimeFlex

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Hero } from '../../interfaces/hero.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { delay, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
   styles: ``
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+  // 1. Creamos una propiedad pública para almacenar el héroe obtenido desde el servicio, el simbolo "?" indica que la propiedad puede ser nula
+  public hero?: Hero;
+  // 2. Inyectamos el servicio HeroesService, ActivatedRoute y Router en el constructor de la clase HeroPageComponent
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
+  // 3. Implementamos el método ngOnInit() para obtener el id del héroe de la ruta actual y luego obtener los detalles del héroe utilizando el servicio HeroesService
+  ngOnInit(): void {
+    // 4. Utilizamos el método params del objeto activatedRoute para obtener el id de la ruta actual. Luego, utilizamos el operador pipe() para encadenar una serie de operadores de RxJS. El operador switchMap() se utiliza para cambiar de un observable a otro, en este caso, para cambiar del observable params al observable que se devuelve al llamar al método getHeroById() del servicio HeroesService. Esto nos permite obtener los detalles del héroe de forma asíncrona y actualizar la propiedad hero de la clase HeroPageComponent con los detalles obtenidos. Si el héroe no existe, redirigimos al usuario a la página de lista de héroes utilizando el método navigate() del objeto router.
+    this.activatedRoute.params
+      .pipe(
+        //delay(5000), // Agregamos un delay de 3 segundos para simular una carga asíncrona
+        switchMap(({ id }) => this.heroesService.getHeroById(id))
+
+      ).subscribe( hero => { 
+
+        if( !hero ) return this.router.navigate(['/heroes/list']);
+        // 5. Si el héroe existe, actualizamos la propiedad hero de la clase HeroPageComponent con los detalles obtenidos. También imprimimos los detalles del héroe en la consola para fines de depuración.
+        this.hero = hero;
+        console.log({hero});
+        // 6. Finalmente, retornamos undefined para completar la suscripción al observable. Esto es necesario para evitar errores de memoria y para asegurarnos de que la suscripción se cancele cuando el componente se destruya.
+        return;
+       })
+  }
+
+  public goback(): void {
+    this.router.navigateByUrl('heroes/list');
+  }
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,17 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environments';
 import { Hero } from '../interfaces/hero.interface';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class HeroesService {
 
+    // Creamos una proiedad privada para guardar la url base de la api
     private baseUrl: string = environment.baseURL;
 
+    // Inyectamos el HttpClient en el constructor para poder usarlo en los métodos
     constructor(private http: HttpClient) { }
 
+    // 1. Creamos un método para obtener todos los héroes de tipo Observable<Hero[]> que retoorne un array de héroes
     getHeroes(): Observable<Hero[]> {
         return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+    }
+
+    // 2. Creamos un método para obtener un héroe por su id de tipo Observable<Hero | undefined> que retoorne un héroe o undefined si no lo encuentra
+    getHeroById(id: string): Observable<Hero | undefined> {
+        // 3. Usamos el método get de HttpClient para obtener el héroe por su id y usamos el operador pipe para manejar el error si no lo encuentra
+        return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+            .pipe(
+                // 4. Usamos el operador catchError para manejar el error si no lo encuentra y retornamos un observable de tipo Hero | undefined con el valor de undefined
+                catchError(error => of(undefined))
+                // Nota: of es un creador de observables en RxJS. Cuando usas of, genera un observable que emite los valores que le pasas como argumentos, en el orden en el que los proporcionas, y luego se completa. En este caso, estamos creando un observable que emite el valor undefined y luego se completa, lo que significa que cualquier suscriptor recibirá el valor undefined y luego la notificación de que el observable se ha completado.
+            );
     }
     
 }
